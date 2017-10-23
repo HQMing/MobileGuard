0263d2814b98f4005dc3f479d1a2d68a370e8715
HQMing <1106665408@qq.com>
2017-10-23 21:21:00 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,11 @@
+package cn.edu.gdmec.android.mobileguard.m1home.entity;
+
+/**
+ * Created by FGHJK on 2017/10/22.
+ */
+
+public class VersionEntity {
+    public String versioncode;
+    public String description;
+    public String apkurl;
+}
